153d7a5c6667fa56ddc9753b4873f889c7cd5b27
Miguel Angel Bernal <dm.mbernal@gmail.com>
2020-08-06 15:44:41 -0400

    findById

---
+++ app/src/main/java/com/rodrigo/holarodrigo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rodrigo.holarodrigo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
